9d51eb35ea3f1223b8d9e719e61f79385647997b
rustemcoding571 <rustem.huseynov.2016@gmail.com>
2024-07-21 14:36:28 +0400

    Custom exception create and findCustomerById method added project

--- src/main/java/com/rustam/account/service/CustomerService.java
+++ src/main/java/com/rustam/account/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.rustam.account.service;
 
+import com.rustam.account.exception.CustomerNotFoundException;
+import com.rustam.account.model.Customer;
 import com.rustam.account.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +12,9 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
 
+    protected Customer findCustomerById(Long id){
+        return customerRepository.findById(id)
+                .orElseThrow(() -> new CustomerNotFoundException
+                        ("customer could not find by id : " + id));
+    }
 }
